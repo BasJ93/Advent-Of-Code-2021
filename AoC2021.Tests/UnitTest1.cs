0d61e577ef8ca0e03119ecc7b6b8da43844fa033
Bas Janssen <bas@basjanssen.eu>
2021-11-30 08:54:21 +0100

    Setup Initial workspace for AoC2021

---
+++ AoC2021.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace AoC2021.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
